ba4ca079d5a52d6567dd83c79b4bb664f6390dc6
ZR25002 <zr25002@ues.edu.sv>
2025-05-10 18:02:04 -0600

    Parte III Guia

---
+++ com.iis.zr25002/src/main/java/com/iis/zr25002/App.java
@@ -0,0 +1,19 @@
+package com.iis.zr25002;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main(String[] args) {
+        App hw = new App();
+    	System.out.println("Hello World!");
+    }
+}
---
+++ com.iis.zr25002/src/test/java/com/iis/zr25002/AppTest.java
@@ -0,0 +1,19 @@
+package com.iis.zr25002;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() {
+        assertTrue(true);
+    }
+}
